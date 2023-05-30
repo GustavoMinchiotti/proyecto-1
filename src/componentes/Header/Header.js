9a30368cd732978bae34a1fd083d615e33c32ddd
Gustavo M. Minchiotti <Gustavominchiotti@gmail.com>
2023-05-30 15:53:14 -0300

    corrección header

--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -2,7 +2,7 @@ import "./Header.css";
 
 function Header() {
     return <header className="header">
-        <img src="/img/header.png" alt="org"></img>
+        <img src="/img/Header.png" alt="org"></img>
     </header>
 }
 
